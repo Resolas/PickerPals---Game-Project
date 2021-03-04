2386ea742cac46c3d7cb7604692b13dc72429325
JoshKearns <61230934+JoshKearns@users.noreply.github.com>
2021-03-04 13:50:47 +0000

    1

---
+++ PickerPals Game1/Assets/Josh/BeltMove.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BeltMove : MonoBehaviour
+{
+    public float speed;
+
+    public bool onBelt;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        Destroy(gameObject, 20f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (onBelt)
+            transform.position = new Vector3(transform.position.x + speed * Time.deltaTime, transform.position.y, transform.position.z);
+    }
+}
